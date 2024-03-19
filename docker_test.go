881d808cad4d6606889fb4a5f53a234f88ad070b
Joseph Saylor <doug@saylorsolutions.com>
2024-03-18 21:17:44 -0500

    Proper naming of test
     - Adding coverage for getting args.

--- docker_test.go
+++ docker_test.go
@@ -7,9 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDocker(t *testing.T) {
+func TestDocker_DryRun(t *testing.T) {
 	ctx := context.Background()
 	err := Docker().Dry().Command("do", "command").Run(ctx)
 	assert.Error(t, err)
 	assert.Equal(t, "dry run: docker do command", err.Error())
+	assert.Equal(t, []string{"do", "command"}, err.(*DryRunResult).Args())
 }
